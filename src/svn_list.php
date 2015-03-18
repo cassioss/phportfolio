37dff0b699370d3be7bd853de67b471fc79b6587
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-18 17:10:55 -0500

    Added svn_log and svn_list

--- src/svn_list.php
+++ src/svn_list.php
@@ -5,4 +5,4 @@
 
     $svn_repo = "https://subversion.ews.illinois.edu/svn/sp15-cs242/dssntss2";
 
-    print_r(svn_log($svn_repo, null, null, 0, 'SVN_DISCOVER_CHANGED_PATHS'));
+    print_r(svn_log($svn_repo, null, null, 1, 'SVN_DISCOVER_CHANGED_PATHS'));
