aef110f50db42bec4124c6104323429633903859
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-18 17:14:42 -0500

    Added XML paths to PHP file

--- src/svn_list.php
+++ src/svn_list.php
@@ -1,8 +1,6 @@
 <?php
     
     // Path that keeps XML files inside
-    $xml_from_svn = "../resources";
-
+    $svn_log_xml = dirname(__FILE__)."../resources/svn_log.xml";
+    $svn_list_xml = dirname(__FILE__)."../resources/svn_list.xml";
     $svn_repo = "https://subversion.ews.illinois.edu/svn/sp15-cs242/dssntss2";
-
-    print_r(svn_log($svn_repo, null, null, 1, 'SVN_DISCOVER_CHANGED_PATHS'));
