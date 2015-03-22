2ae0d7a5213b728b5a871df0f2070530a485930e
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-22 18:46:43 -0500

    Cleaned up file parsing

--- svn_list.php
+++ svn_list.php
@@ -1,27 +1,44 @@
 <?php
+    
+    function linkToURL($fullPath){
+        return "<a href=\"$fullPath\" target = \"_blank\">$fullPath</a>";
+    }
+
     echo "SVN list data<br><hr><br>";
     
     // Loads each one of the files
     //$file_logs = simplexml_load_file('resources/svn_log.xml');
-    $file_list = simplexml_load_file('resources/svn_list.xml');
+    $fileList = simplexml_load_file('resources/svn_list.xml');
     
-    $list = $file_list->list;
-    $path = $list['path'];
+    $list = $fileList->list;
+    $originPath = $list['path'];
     
-    echo "$path<br><br>";
-    
-    // Parses SVN file list
+    // Parses SVN list file
     foreach ($list->entry as $entry) {
         $kind = $entry['kind'];
-        $name = $entry->name;
+    
+        $relativePath = $entry->name; 
+        $relativePathArray = explode("/", $relativePath);
+        $name = end($relativePathArray);
+
+        $path = $originPath . "/" . $relativePath;
+    
+        $type = "";
+        $namePlusType = explode(".", $name);
+        if(sizeof($namePlusType) > 1)
+            $type = "." . end($namePlusType);
+    
         $revision = $entry->commit['revision'];
+    
         $author = $entry->commit->author;
-        $date = date('m/d/Y', strtotime((string)$entry->commit->date));
+    
         $size = $entry->size;
-        echo "$kind<br>$name<br>$revision<br>$author<br>$date<br>$size<br><br>";
-        $path_as_array = explode("/", $name);
-        
-
-
-
+        if(!is_null($size))
+            $size = 0;
+    
+        $date = date('m/d/Y', strtotime((string)$entry->commit->date));
+    
+        echo "$kind<br>$name<br>$revision<br>$author<br>$date<br>$size bytes<br>$type<br>";
+        echo linkToURL($path);
+        echo "<br><br>";
     }
