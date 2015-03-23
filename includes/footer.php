c695a503a34d6b118b58f0af7bb505fef5cb1386
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-23 00:15:24 -0500

    Removed Javascript

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,3 @@
         </ul>
-        <!-- Bootstrap core JavaScript -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="js/jquery-2.1.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
